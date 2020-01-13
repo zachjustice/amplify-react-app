b56bc1874d989b397383d17e652fe3b0afbb7e81
Zach Justice <zach.j.justice@gmail.com>
2020-01-13 13:33:20 -0500

    Add amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify, { Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, S3Album } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
+Storage.configure({ level: 'private'  });
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`;
+
+class App extends Component {
+    todoMutation = async () => {
+        const todoDetails = {
+            name: 'Party tonight!',
+            description: 'Amplify CLI rocks!'
+        };
+
+        const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+        alert(JSON.stringify(newTodo));
+    };
+
+    listQuery = async () => {
+        console.log('listing todos');
+        const allTodos = await API.graphql(graphqlOperation(listTodos));
+        alert(JSON.stringify(allTodos));
+    };
+
+    uploadFile = (evt) => {
+        const file = evt.target.files[0];
+        const name = file.name;
+
+        Storage.put(name, file).then(() => {
+            this.setState({ file: name  });
+        })
+    }
+
+    componentDidMount() {
+        Analytics.record('Amplify_CLI');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <p> Pick a file</p>
+                <input type="file" onChange={this.uploadFile} />
+                <button onClick={this.listQuery}>GraphQL Query</button>
+                <button onClick={this.todoMutation}>GraphQL Mutation</button>t
+                <S3Album level="private" path='' />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withAuthenticator(App, true);
